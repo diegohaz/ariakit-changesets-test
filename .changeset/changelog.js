ba47c3919d9b03d53e8d4f340040d7da8b83ca14
Haz <hazdiego@gmail.com>
2022-04-07 10:13:30 +0200

    Update changelog format

--- .changeset/changelog.js
+++ .changeset/changelog.js
@@ -1,108 +1,25 @@
-const {
-  getInfo,
-  getInfoFromPullRequest,
-} = require("@changesets/get-github-info");
-
+// @ts-check
 /** @type {import("@changesets/types").ChangelogFunctions} */
 const changelogFunctions = {
-  getDependencyReleaseLine: async (_, dependenciesUpdated, options) => {
-    if (!options.repo) {
-      throw new Error(
-        'Please provide a repo to this changelog generator like this:\n"changelog": ["@changesets/changelog-github", { "repo": "org/repo" }]'
-      );
-    }
+  getDependencyReleaseLine: async (_, dependenciesUpdated) => {
     if (dependenciesUpdated.length === 0) return "";
-
-    const changesetLink = "- Updated dependencies:";
     const updatedDepenenciesList = dependenciesUpdated.map(
-      (dependency) => `  - ${dependency.name}@${dependency.newVersion}`
+      (dependency) => `\`${dependency.name}@${dependency.newVersion}\``
     );
-
-    return [changesetLink, ...updatedDepenenciesList].join("\n");
+    return `- Updated dependencies: ${updatedDepenenciesList.join(", ")}.`;
   },
-  getReleaseLine: async (changeset, type, options) => {
-    if (!options || !options.repo) {
-      throw new Error(
-        'Please provide a repo to this changelog generator like this:\n"changelog": ["@changesets/changelog-github", { "repo": "org/repo" }]'
-      );
+  getReleaseLine: async (changeset) => {
+    const [firstLine, ...nextLines] = changeset.summary
+      .split("\n")
+      .map((l) => l.trimEnd());
+
+    let returnVal = `- ${firstLine}`;
+
+    if (nextLines.length > 0) {
+      returnVal += `\n${nextLines.map((l) => `  ${l}`).join("\n")}`;
     }
 
-    /** @type {number | undefined} */
-    let prFromSummary;
-    /** @type {string | undefined} */
-    let commitFromSummary;
-    /** @type {string[]} */
-    let usersFromSummary = [];
-
-    console.log(changeset.summary);
-
-    const replacedChangelog = changeset.summary
-      .replace(/^\s*(?:pr|pull|pull\s+request):\s*#?(\d+)/im, (_, pr) => {
-        let num = Number(pr);
-        if (!isNaN(num)) prFromSummary = num;
-        return "";
-      })
-      .replace(/^\s*commit:\s*([^\s]+)/im, (_, commit) => {
-        commitFromSummary = commit;
-        return "";
-      })
-      .replace(/^\s*(?:author|user):\s*@?([^\s]+)/gim, (_, user) => {
-        usersFromSummary.push(user);
-        return "";
-      })
-      .trim();
-
-    const [firstLine, ...futureLines] = replacedChangelog
-      .split("\n")
-      .map((l) => l.trimRight());
-
-    const links = await (async () => {
-      if (prFromSummary !== undefined) {
-        let { links } = await getInfoFromPullRequest({
-          repo: options.repo,
-          pull: prFromSummary,
-        });
-        if (commitFromSummary) {
-          links = {
-            ...links,
-            commit: `[\`${commitFromSummary}\`](https://github.com/${options.repo}/commit/${commitFromSummary})`,
-          };
-        }
-        return links;
-      }
-      const commitToFetchFrom = commitFromSummary || changeset.commit;
-      if (commitToFetchFrom) {
-        let { links } = await getInfo({
-          repo: options.repo,
-          commit: commitToFetchFrom,
-        });
-        return links;
-      }
-      return {
-        commit: null,
-        pull: null,
-        user: null,
-      };
-    })();
-
-    const users = usersFromSummary.length
-      ? usersFromSummary
-          .map(
-            (userFromSummary) =>
-              `[@${userFromSummary}](https://github.com/${userFromSummary})`
-          )
-          .join(", ")
-      : links.user;
-
-    const prefix = [
-      links.pull === null ? "" : ` ${links.pull}`,
-      links.commit === null ? "" : ` ${links.commit}`,
-      users === null ? "" : ` Thanks ${users}!`,
-    ].join("");
-
-    return `\n\n-${prefix ? `${prefix} -` : ""} ${firstLine}\n${futureLines
-      .map((l) => `  ${l}`)
-      .join("\n")}`;
+    return returnVal;
   },
 };
 
