f8bbd6c41d28c26b7874b0211d930bc4d23e0c6b
Haz <hazdiego@gmail.com>
2022-04-09 23:42:34 +0200

    Update scripts

--- scripts/release/publish.js
+++ scripts/release/publish.js
@@ -1,9 +1,19 @@
 const spawn = require("cross-spawn");
 
-const output = spawn.sync("npx", ["changeset", "publish"], {
-  stdio: "pipe",
+process.stdout.on("data", (data) => {
+  // const output = data
+  //   .toString()
+  //   .split("\n")
+  //   .filter((line) => line.includes("@"))
+  //   .map((line) => line.trim())
+  //   .join("\n");
+  console.log("aaaa", data, data.toString());
 });
 
-console.log(JSON.stringify(output, null, 2));
+spawn.sync("npx", ["changeset", "publish"], {
+  stdio: "inherit",
+});
+
+// console.log(JSON.stringify(output, null, 2));
 
 // spawn.sync("npm", ["dist-tag", "add"], { stdio: "inherit" });
--- scripts/release/version.js
+++ scripts/release/version.js
@@ -1,5 +1,15 @@
 const spawn = require("cross-spawn");
 
+process.stdout.on("data", (data) => {
+  // const output = data
+  //   .toString()
+  //   .split("\n")
+  //   .filter((line) => line.includes("@"))
+  //   .map((line) => line.trim())
+  //   .join("\n");
+  console.log("aaaa", data, data.toString());
+});
+
 spawn.sync("npx", ["changeset", "version"], {
   stdio: "inherit",
 });
