804bbfe789d5e75ea68dad54f58861e0f29079f0
Haz <hazdiego@gmail.com>
2022-04-10 00:20:18 +0200

    Update scripts

--- scripts/release/publish.js
+++ scripts/release/publish.js
@@ -2,9 +2,21 @@ const spawn = require("cross-spawn");
 
 const changeset = spawn.sync("npx", ["changeset", "publish"]);
 
-console.log("dsadsadas1", changeset.stdout.toString());
-console.log("dsadsadas2", changeset.stderr.toString());
+const releaseTags = process.env.RELEASE_TAGS;
 
-// console.log(JSON.stringify(output, null, 2));
+console.log(releaseTags);
 
-// spawn.sync("npm", ["dist-tag", "add"], { stdio: "inherit" });
+if (!releaseTags) process.exit();
+
+const tags = releaseTags.split(",");
+
+const packages = changeset.stdout
+  .toString()
+  .split("\n")
+  .filter((line) => /^\S+@\S+$/.test(line));
+
+for (const package of packages) {
+  for (const tag of tags) {
+    spawn.sync("npm", ["dist-tag", "add", package, tag], { stdio: "inherit" });
+  }
+}
