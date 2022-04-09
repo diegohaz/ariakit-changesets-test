8dce4f80bb97dcb8714d8c22f84257c5af2d7e9d
Haz <hazdiego@gmail.com>
2022-04-09 23:22:32 +0200

    No audit

--- scripts/release/version.js
+++ scripts/release/version.js
@@ -1,4 +1,6 @@
 const spawn = require("cross-spawn");
 
 spawn.sync("changeset", ["version"], { stdio: "inherit" });
-spawn.sync("npm", ["i", "--package-lock-only"], { stdio: "inherit" });
+spawn.sync("npm", ["i", "--package-lock-only", "--no-audit"], {
+  stdio: "inherit",
+});
