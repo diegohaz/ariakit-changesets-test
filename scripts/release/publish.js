b6b27472c5e156e6f4eeb251b12437637f958373
Haz <hazdiego@gmail.com>
2022-04-09 23:53:30 +0200

    Update scripts

--- scripts/release/publish.js
+++ scripts/release/publish.js
@@ -1,18 +1,9 @@
 const spawn = require("cross-spawn");
 
-process.stdout.on("data", (data) => {
-  // const output = data
-  //   .toString()
-  //   .split("\n")
-  //   .filter((line) => line.includes("@"))
-  //   .map((line) => line.trim())
-  //   .join("\n");
-  console.log("aaaa", data, data.toString());
-});
+const changeset = spawn.sync("npx", ["changeset", "publish"]);
 
-spawn.sync("npx", ["changeset", "publish"], {
-  stdio: "inherit",
-});
+console.log("dsadsadas1", changeset.stdout.toString());
+console.log("dsadsadas2", changeset.stderr.toString());
 
 // console.log(JSON.stringify(output, null, 2));
 
--- scripts/release/version.js
+++ scripts/release/version.js
@@ -1,19 +1,13 @@
 const spawn = require("cross-spawn");
 
-process.stdout.on("data", (data) => {
-  // const output = data
-  //   .toString()
-  //   .split("\n")
-  //   .filter((line) => line.includes("@"))
-  //   .map((line) => line.trim())
-  //   .join("\n");
-  console.log("aaaa", data, data.toString());
-});
-
 spawn.sync("npx", ["changeset", "version"], {
   stdio: "inherit",
 });
 
-spawn.sync("npm", ["i", "--package-lock-only", "--no-audit", "--no-scripts"], {
-  stdio: "inherit",
-});
+spawn.sync(
+  "npm",
+  ["i", "--package-lock-only", "--no-audit", "--ignore-scripts", "--no-fund"],
+  {
+    stdio: "inherit",
+  }
+);
